c7d672957a4cb69a04f8a2b88852026ac214e744
prakriti.bhattarai5 <prakriti.bhattarai5@gmail.com>
2024-08-29 23:09:01 -0600

    Testing the project Deployment

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>This is about page for my next.js app.</h1>
+      <p>
+        Created this project to test the deployment of the project in vercel and
+        implement CI/CD pipeline in next.js.
+      </p>
+    </>
+  );
+}
